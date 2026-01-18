7afe2b9c83ce3c2eb6518398214743efb53dc7c2
Ihor Sukhachov <i.sukhachov@gmail.com>
2026-01-18 13:19:43 +0200

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Ihor Sukhachov on 18.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
