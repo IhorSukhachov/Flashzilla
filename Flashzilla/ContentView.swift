dfee06d40320b9694a15ea521dc570f269e011cf
Ihor Sukhachov <i.sukhachov@gmail.com>
2026-01-18 13:52:14 +0200

    adding rotation gesture

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -10,22 +10,37 @@ import SwiftUI
 struct ContentView: View {
     @State private var currentAmount = 0.0
     @State private var finalAmount = 1.0
+    
+    @State private var currentAngleAmount = Angle.zero
+    @State private var finalAngleAmount = Angle.zero
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
-                .scaleEffect(finalAmount + currentAmount )
+                .scaleEffect(finalAmount + currentAmount)
+                .rotationEffect(currentAngleAmount + finalAngleAmount)
                 .gesture(
-                    MagnifyGesture()
+//                    MagnifyGesture()
+//                        .onChanged {value in
+//                            currentAmount = value.magnification - 1
+//                        }
+//                        .onEnded {value in
+//                            finalAmount += currentAmount
+//                            currentAmount = 0.0
+//                        }
+                    
+                    RotateGesture()
                         .onChanged {value in
-                            currentAmount = value.magnification - 1
+                            currentAngleAmount = value.rotation
                         }
                         .onEnded {value in
-                            finalAmount += currentAmount
-                            currentAmount = 0.0
+                            finalAngleAmount += currentAngleAmount
+                            currentAngleAmount = .zero
                         }
+                        
                 )
         }
         .padding()
