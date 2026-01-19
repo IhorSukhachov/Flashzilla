a70b7ce9d9fad1092c081d2270535992f99dd340
Ihor Sukhachov <i.sukhachov@gmail.com>
2026-01-19 18:02:33 +0200

    one more way to cancel

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -10,17 +10,24 @@ internal import Combine
 
 struct ContentView: View {
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    @State private var count = 0
 
     var body: some View {
         Text("Hello world")
             .onReceive(timer) {time in
-                print("The time is now \(time)")
+                if count == 5 {
+                    timer.upstream.connect().cancel()
+                } else {
+                    print("The time is now \(time)")
+                }
+                count += 1
             }
     }
     
-    func cancelTimer() {
-        timer.upstream.connect().cancel()
-    }
+//    func cancelTimer() {
+//        timer.upstream.connect().cancel()
+//    }
 }
 
 #Preview {
