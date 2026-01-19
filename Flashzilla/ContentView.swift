7d0c3db7729fb5c7cb9a356985ebe7c53873ebea
Ihor Sukhachov <i.sukhachov@gmail.com>
2026-01-19 17:57:40 +0200

    making a timer

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -6,104 +6,18 @@
 //
 
 import SwiftUI
+internal import Combine
 
 struct ContentView: View {
-//    @State private var currentAmount = 0.0
-//    @State private var finalAmount = 1.0
-//    
-//    @State private var currentAngleAmount = Angle.zero
-//    @State private var finalAngleAmount = Angle.zero
-    
-    @State private var offset = CGSize.zero
-    @State private var isDraging = false
-    
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+
     var body: some View {
-        ZStack {
-            Rectangle()
-                .fill(Color.blue)
-                .frame(width: 300, height: 300)
-                .onTapGesture {
-                    print("Rectangle tapped")
-                }
-            Circle()
-                .fill(.red)
-                .frame(width: 300, height: 300)
-                .onTapGesture {
-                    print("Circle tapped")
-                }
-                .allowsHitTesting(false)
-        }
-        
-        
-        
-        
-        
-        
-        
-        
-//        let dragGesture = DragGesture().onChanged({ value in
-//            offset = value.translation
-//        }).onEnded  { _ in
-//            withAnimation {
-//                offset = .zero
-//                isDraging = false
-//            }
-//        }
-//                   
-//        let pressGesture = LongPressGesture()
-//            .onEnded { value in
-//            withAnimation {
-//                isDraging = true
-//            }
-//        }
-//        
-//        let combined = pressGesture.sequenced(before: dragGesture)
-//        
-//        Circle()
-//             .fill(Color.blue)
-//             .frame(width: 64, height: 64)
-//             .scaleEffect(isDraging ? 1.5 : 1.0)
-//             .offset(offset)
-//             .gesture(combined)
-//        
-////        VStack {
-//////            Image(systemName: "globe")
-////                .imageScale(.large)
-////                .foregroundStyle(.tint)
-////            Text("Hello, world!")
-////                .scaleEffect(finalAmount + currentAmount)
-////                .rotationEffect(currentAngleAmount + finalAngleAmount)
-////                .gesture(
-//////                    MagnifyGesture()
-//////                        .onChanged {value in
-//////                            currentAmount = value.magnification - 1
-//////                        }
-//////                        .onEnded {value in
-//////                            finalAmount += currentAmount
-//////                            currentAmount = 0.0
-//////                        }
-////                    
-////                    RotateGesture()
-////                        .onChanged {value in
-////                            currentAngleAmount = value.rotation
-////                        }
-////                        .onEnded {value in
-////                            finalAngleAmount += currentAngleAmount
-////                            currentAngleAmount = .zero
-////                        }
-////                        
-////                )
-//            
-//            Text("For double gestures, use two different ones")
-//                .onTapGesture {
-//                    print("tap on text")
-//                }
-//        }
-//        .simultaneousGesture(TapGesture().onEnded {
-//            print("tap on background")
-//        })
+        Text("Hello world")
+            .onReceive(timer) {time in
+                print("The time is now \(time)")
+            }
     }
-} 
+}
 
 #Preview {
     ContentView()
