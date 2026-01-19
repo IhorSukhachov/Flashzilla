3d381d5ee46cffbcbb42de66ff6f17bcaf39f23d
Ihor Sukhachov <i.sukhachov@gmail.com>
2026-01-19 18:27:42 +0200

    differentiate without color accessibility feature

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -12,20 +12,31 @@ struct ContentView: View {
     let timer = Timer.publish(every: 1, tolerance: 0.5, on: .main, in: .common).autoconnect()
     
     @Environment(\.scenePhase) var scenePhase
+    @Environment(\.accessibilityDifferentiateWithoutColor) var accessibilityDifferentiateWithoutColor: Bool
     
     @State private var count = 0
 
     var body: some View {
-        Text("Hello world")
-            .onChange(of: scenePhase) { oldPhase, newPhase in
-                if newPhase == .active {
-                    print("Active")
-                } else if newPhase == .inactive {
-                    print("Inactive")
-                } else if newPhase == .background {
-                    print("Background")
-                }
+        HStack {
+             if accessibilityDifferentiateWithoutColor {
+                Image(systemName: "checkmark.circle")
             }
+            Text("Success")
+        }
+        .padding()
+        .background(accessibilityDifferentiateWithoutColor ? .black : .green)
+        .foregroundStyle(.white)
+        .clipShape(.capsule)
+//        Text("Hello world")
+//            .onChange(of: scenePhase) { oldPhase, newPhase in
+//                if newPhase == .active {
+//                    print("Active")
+//                } else if newPhase == .inactive {
+//                    print("Inactive")
+//                } else if newPhase == .background {
+//                    print("Background")
+//                }
+//            }
 //            .onReceive(timer) {time in
 //                if count == 5 {
 //                    timer.upstream.connect().cancel()
